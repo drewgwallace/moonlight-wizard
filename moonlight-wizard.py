ce68b2028d0fb2fcfab7d7f31a0da37735e45d3e
Drew G. Wallace <32915923+drewgwallace@users.noreply.github.com>
2017-10-23 16:37:05 -0700

    initial upload of rough draft
    incomplete work

---
+++ moonlight-wizard.py
@@ -0,0 +1,27 @@
+
+import os
+import distutils.dir_util
+
+distutils.dir_util.mkpath('/home/pi/RetroPie/roms/moonlight')
+
+
+print("Configuring Moonlight as system")
+if 'Moonlight' in open('/etc/emulationstation/es_systems.cfg').read():
+    print("    Moonlight system exists")
+else:
+    print("    Adding system!")
+    with open('/etc/emulationstation/es_systems.cfg', 'a') as moon_cfg:
+        moon_cfg.write("  <system>\n")
+        moon_cfg.write("    <name>Moonlight</name>\n")
+        moon_cfg.write("    <path>/home/pi/RetroPie/roms/moonlight/</path>\n")
+        moon_cfg.write("    <extension>.sh</extension>\n")
+        moon_cfg.write("    <command>%ROM%</command>\n")
+        moon_cfg.write("    <theme>moonlight</theme>\n")
+        moon_cfg.write("  </system>\n")
+
+distutils.dir_util.mkpath('/etc/emulationstation/themes/carbon/moonlight')
+distutils.dir_util.mkpath('/etc/emulationstation/themes/carbon/moonlight/art')
+
+# check that ./themes folder exists
+# cp ./themes/carbon/moonlight/theme.xml to /etc/emulationstation/themes/carbon/moonlight
+# cp ./themes/carbon/moonlight/art/* to /etc/emulationstation/themes/carbon/moonlight/art/
